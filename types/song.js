5e0745b721119699689b6b58acdec2c41cc197c7
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-10-23 01:02:02 +0000

    Even better song type

--- types/song.js
+++ types/song.js
@@ -5,13 +5,19 @@ module.exports = (phrase, msg) => {
 	if (songs.size === 1) return songs.first();
 	const exactSongs = songs.filter(nameFilterExact(search));
 	if (exactSongs.size === 1) return exactSongs.first();
+	const artistSongs = songs.filter(nameFilterArtistInexact(search));
+	if (artistSongs.size === 1) return artistSongs.first();
 	return null;
 };
 
-function nameFilterExact(search) {
+function nameFilterArtistInexact(search) {
 	return thing => search.includes(thing.title.toLowerCase()) && search.includes(thing.artist.toLowerCase());
 }
 
 function nameFilterInexact(search) {
 	return thing => thing.title.toLowerCase().includes(search);
+}
+
+function nameFilterExact(search) {
+	return thing => thing.title.toLowerCase() === search;
 }
